558e52937787717e07442ff9dcccc93031d16ef3
lucas obes <lucaspodtiobes@gmail.com>
2025-07-14 20:35:11 -0300

    Criadas classes iniciais de teste

---
+++ src/main/java/br/furb/Main.java
@@ -0,0 +1,13 @@
+package br.furb;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String bla() {
+        return "bla";
+    }
+}
---
+++ src/test/java/br/furb/MainTests.java
@@ -0,0 +1,9 @@
+package br.furb;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTests {
+
+
+}
